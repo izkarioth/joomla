874a46f01f0f44601b7a685ea44ef1cedd5c781f
izkarioth <100462356+izkarioth@users.noreply.github.com>
2026-07-16 04:11:55 +0200

    Delete default.php

--- default.php
+++
@@ -1,8 +0,0 @@
-<?php
-// no direct access
-defined('_JEXEC') or die;
-?>
-<div class="mod_random_html <?php echo $moduleclass_sfx; ?>">	
-	<?php echo '<div class="random_html">'.$ext_quotes[$ext_random_number].'</div>';?>
-	<div style="clear: both;"></div>
-</div>
